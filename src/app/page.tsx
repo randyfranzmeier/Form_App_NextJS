9e4aae3e96b884e02a7b12180307fb33775264d7
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-09-13 21:39:33 -0500

    deleted link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="items-center justify-center font-mono  flex">
-  
-      <h2><Link href="./@toast/page">toast page</Link></h2>
         <UserForm></UserForm>
       </div>
     </main>
