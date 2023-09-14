b28a41621bb00632c2f0872334d838085f5e43f5
Randy.F <randy.franzmeier@my.uwrf.edu>
2023-09-13 21:37:47 -0500

    Testing nextLink

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image'
 import UserForm from './components/page'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import Link from 'next/link'
+import Toast from './@toast/page'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="items-center justify-center font-mono  flex">
+  
+      <h2><Link href="./@toast/page">toast page</Link></h2>
         <UserForm></UserForm>
       </div>
     </main>
